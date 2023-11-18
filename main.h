a22c0993138b6189eec74dddc57cd48e3005abcd
kevinocholah <kevinocholah@gmail.com>
2023-11-17 18:28:05 -0800

    updated resolve

--- main.h
+++ main.h
@@ -71,14 +71,14 @@ typedef struct specifier
 int _puts(char *str);
 int _putchar(int c);
 
-/* print_functions.c module */
+/* troisieme.c module */
 int print_char(va_list ap, params_t *params);
 int print_int(va_list ap, params_t *params);
 int print_string(va_list ap, params_t *params);
 int print_percent(va_list ap, params_t *params);
 int print_S(va_list ap, params_t *params);
 
-/* number.c module */
+/* conversion.c module */
 char *convert(long int num, int base, int flags, params_t *params);
 int print_unsigned(va_list ap, params_t *params);
 int print_address(va_list ap, params_t *params);
@@ -90,31 +90,31 @@ int get_flag(char *s, params_t *params);
 int get_modifier(char *s, params_t *params);
 char *get_width(char *s, params_t *params, va_list ap);
 
-/* convert_number.c module */
+/* deuxieme.c module */
 int print_hex(va_list ap, params_t *params);
 int print_HEX(va_list ap, params_t *params);
 int print_binary(va_list ap, params_t *params);
 int print_octal(va_list ap, params_t *params);
 
-/* simple_printers.c module */
+/* 0-num.c module */
 int print_from_to(char *start, char *stop, char *except);
 int print_rev(va_list ap, params_t *params);
 int print_rot13(va_list ap, params_t *params);
 
-/* print_number.c module */
+/* 1-num.c module */
 int _isdigit(int c);
 int _strlen(char *s);
 int print_number(char *str, params_t *params);
 int print_number_right_shift(char *str, params_t *params);
 int print_number_left_shift(char *str, params_t *params);
 
-/* params.c module */
+/* 0-init.c module */
 void init_params(params_t *params, va_list ap);
 
-/* string_fields.c modoule */
+/* le_precision.c modoule */
 char *get_precision(char *p, params_t *params, va_list ap);
 
-/* _prinf.c module */
+/* 0-prinf.c module */
 int _printf(const char *format, ...);
 
 #endif /*_MAIN_H_*/
