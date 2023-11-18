c1b5e04b8794f6421d84ae3563519d482aa3ec20
kevinocholah <kevinocholah@gmail.com>
2023-11-17 17:15:54 -0800

    they are all connected

--- specifier.c
+++ specifier.c
@@ -1,79 +1,142 @@
 #include "main.h"
 
 /**
- * print_from_to - prints a range of char addresses
- * @start: starting address
- * @stop: stopping address
- * @except: except address
- *
- * Return: number bytes printed
+ * get_specifier - finds the format function
+ * @s: string of the format
+ * Return: the number of bytes printed
  */
-int print_from_to(char *start, char *stop, char *except)
+
+int (*get_specifier(char *s))(va_list ap, params_t *params)
+
 {
-	int sum = 0;
+	specifier_t specifiers[] = {
+		{"c", print_char},
+		{"d", print_int},
+		{"i", print_int},
+		{"s", print_string},
+		{"%", print_percent},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"u", print_unsigned},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"p", print_address},
+		{"S", print_S},
+		{"r", print_rev},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
 
-	while (start <= stop)
+	int i = 0;
+
+	while (specifiers[i].specifier)
 	{
-		if (start != except)
-			sum += _putchar(*start);
-		start++;
-	}
-	return (sum);
-}
-
-/**
- * print_rev - prints string in reverse
- * @ap: string
- * @params: the parameters struct
- *
- * Return: number bytes printed
- */
-int print_rev(va_list ap, params_t *params)
-{
-	int len, sum = 0;
-	char *str = va_arg(ap, char *);
-	(void)params;
-
-	if (str)
-	{
-		for (len = 0; *str; str++)
-			len++;
-		str--;
-		for (; len > 0; len--, str--)
-			sum += _putchar(*str);
-	}
-	return (sum);
-}
-
-/**
- * print_rot13 - prints string in rot13
- * @ap: string
- * @params: the parameters struct
- *
- * Return: number bytes printed
- */
-int print_rot13(va_list ap, params_t *params)
-{
-	int i, index;
-	int count = 0;
-	char arr[] =
-		"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
-	char *a = va_arg(ap, char *);
-	(void)params;
-
-	i = 0;
-	index = 0;
-	while (a[i])
-	{
-		if ((a[i] >= 'A' && a[i] <= 'Z')
-		    || (a[i] >= 'a' && a[i] <= 'z'))
+		if (*s == specifiers[i].specifier[0])
 		{
-			index = a[i] - 65;
-			count += _putchar(arr[index]);
+			return (specifiers[i].f);
 		}
-		else
-			count += _putchar(a[i]);
 		i++;
 	}
-	return (count);
+	return (NULL);
+}
+
+/**
+ * get_print_func - finds the format function
+ * @s: string of the format
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * Return: the number of bytes printed
+ */
+
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+
+/**
+ * get_flag - finds the flag functions
+ * @s: the format string
+ * @params: the parameters struct
+ * Return: if flag was valid
+ */
+
+int get_flag(char *s, params_t *params)
+
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * get_modifier - finds the modifier function
+ * @s: string for format
+ * @params: parameter structure
+ * Return: if modifier was valid
+ */
+
+int get_modifier(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case 'h':
+			i = params->h_modifier = 1;
+			break;
+		case 'l':
+			i = params->l_modifier = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * get_width - gets the width from the format string
+ * @s: the format string
+ * @params: the parameters struct
+ * @ap: the argument pointer
+ * Return: new pointer
+ */
+
+char *get_width(char *s, params_t *params, va_list ap)
+
+	/* should this function use char **s and modify the pointer? */
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
 }
